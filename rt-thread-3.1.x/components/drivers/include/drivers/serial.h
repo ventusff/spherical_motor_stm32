63bcab5fd75de072e46b7899df9ef02921ee4a92
Jianfei (Ventus) Guo <ffventus@gmail.com>
2019-02-08 18:38:30 +0800

    feat set default baudrate to 460800

--- rt-thread-3.1.x/components/drivers/include/drivers/serial.h
+++ rt-thread-3.1.x/components/drivers/include/drivers/serial.h
@@ -80,7 +80,7 @@
 /* Default config for serial_configure structure */
 #define RT_SERIAL_CONFIG_DEFAULT           \
 {                                          \
-    BAUD_RATE_115200, /* 115200 bits/s */  \
+    BAUD_RATE_460800, /* 460800 bits/s */   //ventus: changed to 460800 (was 115200) \
     DATA_BITS_8,      /* 8 databits */     \
     STOP_BITS_1,      /* 1 stopbit */      \
     PARITY_NONE,      /* No parity  */     \
